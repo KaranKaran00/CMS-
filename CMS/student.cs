050f158d8403f072f753fdc6e46612a01e7b2c20
KaranKaran00 <kharakaran504@gmail.com>
2026-03-18 20:43:57 +0530

    student login

---
+++ CMS/student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CMS
+{
+    public partial class student : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=D:\SEM-4\Programming with C#\MAM program\CMS\CMS\cms.mdf;Integrated Security = True");
+        public student()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string q = "insert into student(studentname,moblie,email,gender) values('"+textBox1.Text+"',"+textBox2.Text+",'"+textBox3.Text+"')";
+            SqlCommand cmd = new SqlCommand(q, con);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Record Inserted");
+            con.Close();
+        }
+    }
+}
